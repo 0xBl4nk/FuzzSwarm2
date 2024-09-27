85f1ebfc71824c14ba6954cbdba9bba9bdecce72
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2024-09-27 19:44:18 -0300

    Add some config parsers

--- src/config.go
+++ src/config.go
@@ -1,21 +1,26 @@
 package src
 
 import (
-	//"strings"
 	"errors"
+	"fmt"
 	"strings"
-
 	"net/url"
 
 	"github.com/spf13/cobra"
 )
 
-
+// Configuration settings for the fuzzing process.
 type Config struct {
-  URL       string
-  Headers   string
-  Method    string
-  Data      string
+  URL           string
+  Method        string
+  Data          string
+  Range         string
+  Wordlist      string
+  Value         []string
+  
+  HeadersFile   map[string]string
+  HeadersPath   string
+
 }
 
 func LoadConfig(cmd *cobra.Command) (Config, error) {
@@ -23,24 +28,66 @@ func LoadConfig(cmd *cobra.Command) (Config, error) {
 
   cfg.URL, _ = cmd.Flags().GetString("url")
   cfg.Data, _ = cmd.Flags().GetString("data")
+  cfg.Range, _ = cmd.Flags().GetString("range")
   cfg.Method, _ = cmd.Flags().GetString("method")
-  cfg.Headers, _ = cmd.Flags().GetString("headers")
+  cfg.Wordlist, _ = cmd.Flags().GetString("wordlist")
+  cfg.HeadersPath, _ = cmd.Flags().GetString("headers-file")
+  
+ return cfg, nil
+}
 
-  // URL Validate / Parser
+func ParseConfig(cfg Config) (Config, error) {
+  
+  // URL Validate.
   if cfg.URL == "" {
     return cfg, errors.New("the --url flag is required")
   }
-  
-  parsedURL, err := url.Parse(cfg.URL)
-    if err != nil || !(parsedURL.Scheme == "http" || parsedURL.Scheme == "https") {
-      return cfg, errors.New("invalid URL format. Ensure it starts with http:// or https://")
-    }
 
-  // Validate HTTP Method
+  // URL Parsing.
+  parsedURL, err := url.Parse(cfg.URL)
+  if err != nil || !(parsedURL.Scheme == "http" || parsedURL.Scheme == "https") {
+    return cfg, errors.New("invalid URL format. Ensure it starts with http:// or https://")
+  }
+
+  // Validate HTTP Method.
   cfg.Method = strings.ToUpper(cfg.Method)
   if cfg.Method != "GET" && cfg.Method != "POST" {
     return cfg, errors.New("Invalid HTTP method. Only GET and POST are supported")
   }
+
+  // If method is POST, data must be provided.
+  if cfg.Method == "POST" && cfg.Data == "" {
+    return cfg, errors.New("Post method requires --data flag")
+  }
+
+  // Validate Headers File/Path.
+  if cfg.HeadersPath != "" {
+    headersFile, err := ReadHeaders(cfg.HeadersPath)
+    if err != nil {
+      return cfg, fmt.Errorf("error reading headers: %v", err)
+    }
+    cfg.HeadersFile = headersFile
+  }
+
+  // Validate Wordlist and Range.
+  if cfg.Wordlist != "" {
+    wordlistValues, err := ReadWordlistValues(cfg.Wordlist)
+    if err != nil {
+      return cfg, fmt.Errorf("error reading wordlist: %v", err)
+    }
+    cfg.Value = wordlistValues
+
+  } else if cfg.Range != "" {
+    rangeValues, err := parseRange(cfg.Range)
+    
+    if err != nil {
+      return cfg, fmt.Errorf("error parsing range: %v", err)
+    } 
+    cfg.Value = rangeValues
+
+  } else {
+      return cfg, errors.New("either a range or wordlist must be provided")
+    }
 
   return cfg, nil
 }
